ede7e8283610e6a2911c810d546744515944c605
patriotpie <patriotpie@gmail.com>
2014-03-05 12:33:06 -0500

    Stylized results panel, open diff in separate tab

--- data/get-edit-summaries.js
+++ data/get-edit-summaries.js
@@ -1,23 +1,39 @@
 self.port.on("getEditSummaries", function() {
 
 	var summaries = [];
-	var pageHistory_ul = document.getElementById("pagehistory");
+	var pageHistoryEdits = document.getElementById("pagehistory").querySelectorAll("li");
 
-	try {
-	for (var i = 1; i < pageHistory_ul.childNodes.length; i += 2) {
-		td = pageHistory_ul.childNodes[i].getElementsByClassName("comment");
+	//try {
+	for (var i = 0; i < pageHistoryEdits.length; i++) {
+
+		var td = pageHistoryEdits[i].getElementsByClassName("comment");
+
 		if (td.length == 1 &&
-		   (td[0].childNodes.length == 1 ||
-				td[0].getElementsByClassName("autocomment").length == 0)) {
+			// We don't care about autocomments!
+		   (td[0].getElementsByClassName("autocomment").length == 0)) {
 
-			summaries.push(td[0].textContent + "\n");
+			if (i != 0) {
+				summaries.push({
+					sum: td[0].textContent,
+					link: pageHistoryEdits[i].getElementsByClassName("mw-history-histlinks")[0].childNodes[1].href
+				});
+			}
+			else {
+				// This is the current edit and therefore does not have a "cur" link
+				summaries.push({
+					sum: td[0].textContent,
+					link: null
+				});
+			}
 
 		}
 	}
 
 	self.port.emit("editSummariesReady", summaries);
-	} catch (e) {
-		self.port.emit("editSummariesError", e);
-	}
+
+
+	//} catch (e) {
+	//	self.port.emit("editSummariesError", e);
+	//}
 
 });
--- data/panel.js
+++ data/panel.js
@@ -1,23 +1,61 @@
-self.port.on("setHTML", function(setHTML) {
-	document.innerHTML = setHTML;
-});
+var bgColors = {
+	editSums: "#FF9999",
+	userPages: "#E6E6AF",
+	talkPage : "#80CCE6"
+};
+
+var interestingEditSummaries = "";
+var interestingUserPageSections = "";
+var interestingTalkPageSections = "";
 
 self.port.on("setInterestingEditSummaries", function(editSummariesJson) {
 
-
-	document.body.innerHTML = "";
-
 	if (editSummariesJson.length == 0) {
-		document.body.style = "text-align: left; padding-left: 100px;";
-		document.body.innerHTML = "<p>No interesting edit summaries.</p>";
+		interestingEditSummaries = "No interesting edit summaries.";
 	}
 
 	else {
-		document.body.style = "";
+		var emitted = false;
+
+		interestingEditSummaries = "";
 		for (var i = 0; i < editSummariesJson.length; i++) {
-			var summary = document.createElement("p");
-			summary.innerHTML = "<b>" + editSummariesJson[i].editSummary + ":</b> " + editSummariesJson[i].desc + "\n";
-			document.body.appendChild(summary);
+
+			interestingEditSummaries += "<h3 style=\"font-weight:bold; font-size: 9pt;\">"
+					+ "<span style=\"font-color:blue; text-decoration:underline; cursor:pointer;\" "
+					+ "loc='" + editSummariesJson[i].link + "'>[Edit summary]</a></h3>"
+					+ "<p style=\"font-size: 8pt;\">" + editSummariesJson[i].sum + "</p>\n";
+
 		}
 	}
-})
+
+	buildContentView();
+});
+
+window.addEventListener("click", function(event) {
+	var tgt = event.target;
+	if (tgt.nodeName == "SPAN" &&
+		tgt.getAttribute("loc") != null) {
+		self.port.emit("tabOpen", tgt.getAttribute("loc"));
+	}
+});
+
+var buildContentView = function() {
+
+	var editSumsDiv = document.createElement("div");
+	editSumsDiv.style.backgroundColor = bgColors.editSums;
+	editSumsDiv.innerHTML = interestingEditSummaries;
+
+	var userPagesDiv = document.createElement("div");
+	userPagesDiv.style.backgroundColor = bgColors.userPages;
+	userPagesDiv.innerHTML = interestingUserPageSections;
+
+	var talkPageDiv = document.createElement("div");
+	talkPageDiv.style.backgroundColor = bgColors.talkPage;
+	talkPageDiv.innerHTML = interestingTalkPageSections;
+
+	document.body.innerHTML = "";
+	document.body.appendChild(editSumsDiv);
+	document.body.appendChild(userPagesDiv);
+	document.body.appendChild(talkPageDiv);
+
+};
