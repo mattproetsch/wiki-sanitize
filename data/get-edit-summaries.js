85ab5d6bbe874ab182133ac52ba467b68da57022
patriotpie <patriotpie@gmail.com>
2014-03-10 09:32:23 -0400

    Moved edit summary processing to page worker

--- data/get-edit-summaries.js
+++ data/get-edit-summaries.js
@@ -1,9 +1,13 @@
-self.port.on("getEditSummaries", function() {
+var phrases;
 
+self.port.on("getEditSummaries", function(interestingPhrases) {
+
+	phrases = interestingPhrases;
+	
 	var summaries = [];
 	var pageHistoryEdits = document.getElementById("pagehistory").querySelectorAll("li");
 
-	//try {
+	try {
 	for (var i = 0; i < pageHistoryEdits.length; i++) {
 
 		var td = pageHistoryEdits[i].getElementsByClassName("comment");
@@ -11,17 +15,19 @@ self.port.on("getEditSummaries", function() {
 		if (td.length == 1 &&
 			// We don't care about autocomments!
 		   (td[0].getElementsByClassName("autocomment").length == 0)) {
+		   	
+		   	var editSummary = td[0].textContent;
 
-			if (i != 0) {
+			if (i != 0 && testPhrase(editSummary)) {
 				summaries.push({
-					sum: td[0].textContent,
+					sum: editSummary,
 					link: pageHistoryEdits[i].getElementsByClassName("mw-history-histlinks")[0].childNodes[1].href
 				});
 			}
-			else {
+			else if (testPhrase(editSummary) {
 				// This is the current edit and therefore does not have a "cur" link
 				summaries.push({
-					sum: td[0].textContent,
+					sum: editSummary,
 					link: null
 				});
 			}
@@ -32,8 +38,20 @@ self.port.on("getEditSummaries", function() {
 	self.port.emit("editSummariesReady", summaries);
 
 
-	//} catch (e) {
-	//	self.port.emit("editSummariesError", e);
-	//}
+	} catch (e) {
+		self.port.emit("editSummariesError", e);
+	}
 
 });
+
+
+var testPhrase = function(editSummary) {
+
+	for (var i = 0; i < phrases.length; i++) {
+		if (phrases[i].re.test(editSummary)) {
+			return true;
+		}
+	}
+	
+	return false;
+}
